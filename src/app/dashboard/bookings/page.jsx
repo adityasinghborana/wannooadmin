076bc8c26833dfd7dc36638aecb5c434143836f2
aditya singh borana <aditya@ogresto.com>
2024-07-09 02:07:02 +0530

    bookings details completed

--- src/app/dashboard/bookings/page.jsx
+++ src/app/dashboard/bookings/page.jsx
@@ -1,24 +1,30 @@
-'use client'
+"use client";
 import DataGridContainer from "@/app/ui/dashboard/DataGridContainer/DataGridContainer";
+import Link from "next/link";
 import Container from "@/app/ui/dashboard/container/Container";
 import { GetAllBookings } from "@/lib/services";
 import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { MdDelete } from "react-icons/md";
-
+import { MdDelete, MdVisibility } from "react-icons/md";
 
 const Bookings = () => {
+  const router = useRouter();
   // const users = useAppSelector((state) => state.user.Users);
   const [rows, setRows] = useState([]);
   const [selectedRows, setSelectedRows] = useState([]);
 
   useEffect(() => {
-    const getBookings = async()=>{
-       let data = await GetAllBookings()
-       setRows(data)
-    } 
+    const getBookings = async () => {
+      let data = await GetAllBookings();
+      setRows(data);
+    };
     getBookings();
   }, []);
+
+  const handleOpenView = (bookingId) => {
+    router.push(`/dashboard/bookings/${bookingId}`);
+  };
 
   const handleDeleteSelectedRows = () => {
     const updatedRows = rows.filter((row) => !selectedRows.includes(row.id));
@@ -32,27 +38,27 @@ const Bookings = () => {
     {
       field: "bookingId",
       headerName: "Booking_Id",
-      flex: 1,      
+      flex: 1,
     },
     {
       field: "serviceUniqueId",
       headerName: "Service_Unique_Id",
-      flex: 1,      
+      flex: 1,
     },
     {
       field: "serviceType",
       headerName: "Service Type",
-      flex: 1,      
+      flex: 1,
     },
     {
       field: "bookingResultId",
       headerName: "Booking_Result_Id",
-      flex: 1,      
+      flex: 1,
     },
     // {
     //   field: "bookingResult",
     //   headerName: "Reference_No",
-    //   flex: 1,      
+    //   flex: 1,
     // },
     {
       field: "bookingResult",
@@ -66,23 +72,32 @@ const Bookings = () => {
       field: "userId",
       headerName: "User_Id",
       flex: 1,
+      renderCell: (params) => <span>{params?.row?.bookingResult?.userId}</span>,
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      flex: 1,
       renderCell: (params) => (
-        <span>{params?.row?.bookingResult?.userId}</span>
+        <div>
+          <Button
+            onClick={() => handleOpenDialog(params.row)}
+            className="flex mt-3 items-center justify-center px-2 py-1 rounded bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:bg-red-600"
+          >
+            <MdDelete />
+          </Button>
+          <Button
+            onClick={() => handleOpenView(params.row.bookingId)}
+            className="flex mt-3 items-center justify-center px-2 py-1 rounded bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:bg-red-600"
+          >
+            {/* <Link href={`/dashboard/bookings/${params.row.bookingId}`}>
+          
+            </Link> */}
+            <MdVisibility />
+          </Button>
+        </div>
       ),
     },
-    // {
-    //   field: "actions",
-    //   headerName: "Actions",
-    //   flex: 1,
-    //   renderCell: (params) => (
-    //     <Button
-    //       onClick={() => handleOpenDialog(params.row)}
-    //       className="flex mt-3 items-center justify-center px-2 py-1 rounded bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:bg-red-600"
-    //     >
-    //       <MdDelete />
-    //     </Button>
-    //   ),
-    // },
   ];
 
   return (
@@ -93,5 +108,3 @@ const Bookings = () => {
 };
 
 export default Bookings;
-
- 
