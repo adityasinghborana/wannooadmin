e8d0528526956a3f4511c96f3c6929e58560761f
Arsalaan Khan <arslaan505@gmail.com>
2024-07-15 13:49:19 +0530

    changes

--- src/app/dashboard/about-us/page.tsx
+++ src/app/dashboard/about-us/page.tsx
@@ -1,182 +1,142 @@
-"use client"
-import { getAboutUsDetails } from "@/lib/services"
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, debounce } from "@mui/material"
-import { DataGrid, GridColDef, GridDeleteIcon, GridRowId } from "@mui/x-data-grid"
-import { useEffect, useState } from "react"
-import { MdDelete } from "react-icons/md"
+"use client";
+import Container from "@/app/ui/dashboard/container/Container";
+import SCustomImageUpload from "@/app/ui/dashboard/SingleImageLibrary/SCustomImageUpload";
+import Modal from "@/components/model";
+import { GetAllImages, getAboutUsDetails, updateAboutUsDetails } from "@/lib/services";
+import { ChangeEvent, useEffect, useState } from "react";
 
-
-interface Row {
-  id: GridRowId;
-  email: string;
-  firstName: string;
+interface HomePageData {
+  id: string; // Assuming 'id' is a string, adjust as per your actual data type
+  // Define other fields here
+  [key: string]: any; // Allow any other keys with any value type
 }
 
-const Aboutus = () => {
-  const [aboutus, setAboutUs] = useState<Row[]>([]);
-  const [selectedRows, setSelectedRows] = useState<GridRowId[]>([]);
-  const [selectedRow, setSelectedRow] = useState<Row | null>(null);
-  const [open, setOpen] = useState(false);
-  const [filteredRows, setFilteredRows] = useState<Row[]>(aboutus);
-    useEffect(()=>{
-        const getData= async ()=>{
-          let res = await getAboutUsDetails()  
-            setAboutUs([res])
-            setFilteredRows([res])
-        }
-        getData()
-    },[])
+const Home: React.FC = () => {
+  const [aboutUsData, setAboutUsPageData] = useState<HomePageData>({
+    id: "" /* Initialize other fields here */,
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
+  const [isSuccess, setIsSuccess] = useState(false); // State to manage modal visibility
+  const [imagepath, setImagePath] = useState('');
+  const [imagepath2, setImagePath2] = useState('');
+  const [imagepath3, setImagePath3] = useState('');
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getAboutUsDetails();
+      setAboutUsPageData(data);
+    };
+    fetchData();
+  }, []);
 
-    const columns: GridColDef<(typeof aboutus)[number]>[] = [
-      { field: "id", headerName: "ID", flex: 1 },
-      {
-        field: "title",
-        headerName: "Name",
-        flex: 1,
-        editable: true,
-      },
-      {
-        field: "subtitle",
-        headerName: "Subtitle",
-        type: "number",
-        flex: 1,
-        editable: true,
-      },
-      {
-        field: "heading1",
-        headerName: "heading1",
-        flex: 1,
-        editable: true,
-      },
-      {
-        field: "heading2",
-        headerName: "heading2",
-        flex: 1,
-        editable: true,
-      },
-      {
-        field: "subheading2",
-        headerName: "subheading2",
-        flex: 1,
-        editable: true,
-      },
-      {
-        field: "text",
-        headerName: "Text",
-        flex: 1,
-        editable: true,
-      },
-      {
-        field: "actions",
-        headerName: "Actions",
-        flex: 1,
-        renderCell: (params) => {
-          return (
-            <button
-              onClick={() => handleOpenDialog(params.row)}
-              className="flex mt-3 items-center justify-center px-2 py-1 rounded bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:bg-red-600"
-            >
-              <MdDelete />
-            </button>
-          );
-        },
-      },
-    ];
-  
-    const handleOpenDialog = (row?: any) => {
-      row && setSelectedRow(row);
-      setOpen(true);
-    };
-  
-    const handleCloseDialog = () => {
-      setSelectedRow(null);
-      setOpen(false);
-    };
-  
-    const handleDeleteRow = (id: any) => {
-      console.log("Button clicked for ID:", id);
-      handleCloseDialog();
-      // Handle button click event here, you can access the id of the row
-    };
-  
-    const handleDeleteSelectedRows = () => {
-      // const updatedRows = rows.filter(row => !selectedRows.includes(row.id));
-      // setRows(updatedRows);
-      setSelectedRows([]);
-      console.log("Deleted selected rows:", selectedRows);
-      handleCloseDialog();
-      // Perform any additional delete actions here
-    };
-  
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      handleSearch(event.target.value);
-    };
-  
-    const handleSearch = debounce((value: string) => {
-      const filtered = aboutus.filter((row) =>
-        row.email.toLowerCase().includes(value.toLowerCase())
-      );
-      setFilteredRows(filtered);
-    }, 300);
+  useEffect(() => {
+    setAboutUsPageData((prevData) => ({
+      ...prevData,
+      imagepath: imagepath,
+      imagepath2: imagepath2,
+      imagepath3: imagepath3,
+    }));
+  }, [imagepath, imagepath2, imagepath3]);
 
-    return (
-      <div className="mt-5">
-      <Box  sx={{ width: "100%", backgroundColor: "white", borderRadius: 3}}>
-      <div className="mb-4 ml-4 flex justify-between">
-          <TextField
-            label="Search by email"
-            variant="standard"
-            onChange={handleInputChange}
-            className="my-2 rounded-full ml-4"
-          />
-          <Button
-            onClick={() => handleOpenDialog()}
-            disabled={selectedRows.length === 0}
-            className={`${
-              selectedRows.length <= 1 && "hidden"
-            } mt-2 mr-2 px-4 py-2 rounded-3xl bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:bg-red-600`}
-          >
-            <GridDeleteIcon />
-            Delete Selected Rows
-          </Button>
-          <Dialog open={open} onClose={() => handleCloseDialog()}>
-            <DialogTitle>
-              Delete {selectedRow ? "Row" : "Selected Rows"}
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                {selectedRow
-                  ? `Are you sure you want to delete the row with ID ${selectedRow.id}?`
-                  : "Are you sure you want to delete the selected rows?"}
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={() => handleCloseDialog()}>Cancel</Button>
-              <Button
-                onClick={
-                  selectedRow
-                    ? () => handleDeleteRow(selectedRow.id)
-                    : handleDeleteSelectedRows
-                }
-                autoFocus
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setAboutUsPageData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      // Perform form submission logic
+      await updateAboutUsDetails(aboutUsData);
+      setIsSuccess(true); // Set isSuccess to true on successful submission
+    } catch (error) {
+      setIsSuccess(false); // Set isSuccess to false if there's an error
+    } finally {
+      openModal(); // Always open the modal after the submission attempt
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false); // Function to close the modal
+  };
+
+  return (
+    <Container>
+      <div
+        className="bg-white p-8 shadow-md w-full rounded-3xl mt-12 "
+        style={{ height: "calc(100vh - 10rem)" }}
+      >
+        <h2 className="text-2xl text-black font-bold mb-6 text-center">
+          About-us page data
+        </h2>
+        <div
+          className="overflow-y-auto "
+          style={{ maxHeight: "90%", scrollbarWidth: "none" }}
+        >
+          <form onSubmit={handleSubmit} className="grid grid-cols-3 gap-4">
+            {/* Render form inputs based on homepagedata */}
+            {Object.keys(aboutUsData).map((key) => {
+              if (key === "id") return null; // Skip rendering 'id'
+              if(['imagepath', 'imagepath2', 'imagepath3'].includes(key)){
+                return<>
+                {key === 'imagepath' && <SCustomImageUpload onImageSelect={setImagePath} Images={GetAllImages}/> || 
+                key === 'imagepath2' && <SCustomImageUpload onImageSelect={setImagePath2} Images={GetAllImages}/> ||
+                key === 'imagepath3' && <SCustomImageUpload onImageSelect={setImagePath3} Images={GetAllImages}/>
+                }</>                 
+              }
+              return (
+                <div className="mb-4" key={key}>
+                  <label className="block text-gray-700 capitalize">
+                    {key.toUpperCase()}
+                  </label>
+                  <input
+                    type="text"
+                    name={key}
+                    value={aboutUsData[key as keyof HomePageData]}
+                    onChange={handleChange}
+                    className="w-full text-field"
+                  />
+                </div>
+              );
+            })}
+            <div className="flex justify-center col-span-3">
+              <button
+                type="submit"
+                className="bg-primary text-white py-2 px-4 rounded-full hover:bg-purple-400 w-96"
               >
-                Delete
-              </Button>
-            </DialogActions>
-          </Dialog>
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-        <DataGrid
-          rows={filteredRows}
-          columns={columns}
-          autoHeight
-          checkboxSelection
-          onRowSelectionModelChange={(newSelection) => {
-            setSelectedRows(newSelection);
-          }}
-        />
-      </Box>
-    </div>
-  )
-}
+      </div>
 
-export default Aboutus
+      {/* Modal for success message */}
+      {/* Conditional rendering of Modal based on isSuccess */}
+      {isModalOpen && isSuccess && (
+        <Modal
+          isOpen={true} // Set to true to ensure the modal is visible
+          title="Home Page Data"
+          message="Submitted successfully!"
+          onClose={closeModal} // Pass closeModal function to handle modal closure
+        />
+      )}
+
+      {isModalOpen && !isSuccess && (
+        <Modal
+          isOpen={true} // Set to true to ensure the modal is visible
+          title="Error"
+          message="Data did not go through."
+          onClose={closeModal} // Pass closeModal function to handle modal closure
+        />
+      )}
+    </Container>
+  );
+};
+
+export default Home;
